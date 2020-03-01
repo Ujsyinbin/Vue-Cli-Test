851805b48cb7f6c1e1efc7665f932d9666f3672a
BadWoman <505642249@qq.com>
2020-03-01 16:51:09 +0800

    finish todolist
    use vue-cli to finish a todolist

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+	  <input v-model="inputvalue" />
+	  <button @click="submit">Submit</button>
+	  <todo-item v-for="(value,index) in list" :key='index' :value='value' :index='index' @remove="remove(index)"></todo-item>
+  </div>
+</template>
+
+<script>
+import todoitem from './components/todoitem.vue'
+
+export default {
+	components:{
+		'todo-item':todoitem
+	},
+	data:function(){
+		return{
+			inputvalue:'',
+			list:[],
+		}
+	},
+	methods:{
+		submit(){
+			this.list.push(this.inputvalue)
+			this.inputvalue=''
+		},
+		remove(index){
+			this.list.splice(index,1)
+		}
+	},
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/todoitem.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+	{{value}}
+	<button @click="remove">remove</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		props:['value','index'],
+		methods: {
+			remove(){
+				this.$emit('remove',this.index)
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
